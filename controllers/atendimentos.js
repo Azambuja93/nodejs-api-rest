c0b74d113280c4ffe1be6c918e487e7f54866658
Azambuja93 <rafael.estevam.93@gmail.com>
2021-01-25 19:52:59 -0300

    novas rotas e postman

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,5 +1,9 @@
 module.exports = app => {
     app.get('/atendimentos', (req,res) => res.send('Você está na rota de atendimentos e está realizando um get'))
 
-    app.post('/atendimentos', (req,res) => res.send('Você está na rota de atendimento e está realizando um post'))
+    app.post('/atendimentos', (req,res) => {
+    console.log(req.body)
+     res.send('Você está na rota de atendimento e está realizando um post')
+    })
+
 }
