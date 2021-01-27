4405edb83c488128b8add4a24bb235fafd1ba106
Azambuja93 <rafael.estevam.93@gmail.com>
2021-01-27 12:12:30 -0300

    conexão banco mysql com sucesso

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const customExpress = require('./config/customExpress')
+const conexao = require('./infraestrutura/conexao')
 
-const app = customExpress()
+conexao.connect(erro => {
+    if(erro) {
+        console.log(erro)
+    } else {
+        console.log('conectado com sucesso')
 
-app.listen(3000, () => console.log('servidor rodando na porta 3000'))
+        const app = customExpress()
 
-
-
+        app.listen(3000, () => console.log('servidor rodando na porta 3000'))
+    }
+})
