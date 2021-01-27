f37b3a4095fcd90e72af349e40d63678cdd56c8d
Azambuja93 <rafael.estevam.93@gmail.com>
2021-01-27 13:35:45 -0300

    inserindo dados na tebela do banco

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,9 +1,14 @@
+const Atendimento = require('../models/atendimentos')
+
 module.exports = app => {
     app.get('/atendimentos', (req,res) => res.send('Você está na rota de atendimentos e está realizando um get'))
 
     app.post('/atendimentos', (req,res) => {
-    console.log(req.body)
-     res.send('Você está na rota de atendimento e está realizando um post')
+        const atendimento = req.body
+
+        Atendimento.adiciona(atendimento)                                        
+
+        res.send('Post atendimento enviado')
     })
 
 }
