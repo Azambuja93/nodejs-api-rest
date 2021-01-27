48f2bd17cf48fbd2d29673bf978ef1f533a85ce5
Azambuja93 <rafael.estevam.93@gmail.com>
2021-01-27 12:44:08 -0300

    criando tabelas no banco pelo metodo

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const customExpress = require('./config/customExpress')
 const conexao = require('./infraestrutura/conexao')
+const Tabelas = require('./infraestrutura/tabelas')
 
 conexao.connect(erro => {
     if(erro) {
@@ -7,6 +8,8 @@ conexao.connect(erro => {
     } else {
         console.log('conectado com sucesso')
 
+        Tabelas.init(conexao)
+        
         const app = customExpress()
 
         app.listen(3000, () => console.log('servidor rodando na porta 3000'))
